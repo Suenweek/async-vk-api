326d7e370648538eb023281fd6fc9f9b3c7bb10d
Roman Novatorov <roman.novatorov@gmail.com>
2019-02-12 01:20:01 +0300

    - Add unit-tests for `namespace.Namespace` - Use dot access in `test_api`

--- tests/test_api.py
+++ tests/test_api.py
@@ -23,9 +23,9 @@ async def test_api(api):
         )
     except ApiError as exc:
         response = exc.args[0]
-        assert response['method_name'] == 'users.get'
-        assert response['params']['user_ids'] == '1,2,3'
-        assert response['params']['fields'] == 'city,verified'
-        assert 'v' in response['params']
+        assert response.method_name == 'users.get'
+        assert response.params.user_ids == '1,2,3'
+        assert response.params.fields == 'city,verified'
+        assert 'v' in response.params
     else:
         pytest.fail()
