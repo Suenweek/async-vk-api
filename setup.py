d10cdde800e4921e13f6d258d7a9f72bb4eeb37e
Roman Novatorov <roman.novatorov@gmail.com>
2019-04-20 23:50:08 +0300

    Pin `asks` version to `2.2.1`

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='async-vk-api',
-    version='0.6.0',
+    version='0.6.1',
     description='Async VK API built with asks and trio',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/Suenweek/async-vk-api',
     author='Roman Novatorov',
     author_email='roman.novatorov@gmail.com',
-    install_requires=['asks', 'trio']
+    # TODO: Bump version when asks gets session instantiation fixed:
+    #   https://github.com/theelous3/asks/pull/120
+    install_requires=['asks==2.2.1', 'trio']
 )
